87353bc75a833ac62a6165a4dccbcd6aa348c54f
ranjit1024 <ranjitdas2048@gmail.com>
2024-10-24 16:50:41 +0530

    modify dashboard

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -2,295 +2,59 @@ import React from "react";
 
 export default function Dashboard() {
   return (
-    <>
-      <div
-        style={{
-          background: "whitesmoke",
-          minWidth:"100vw",
-          minHeight:"100vh",
-          display: "flex",
-          margin:"10px"
-        }}
-      >
-        
-
-        <div style={{
-            display:"flex",
-            flexWrap:"wrap",
-            flexDirection:"row",
-            width:"100%",
-            height:"100%",
-            // justifyContent:"center",
-            alignItems:"center"
-        }}>
+    <div style={{
+        display:"flex",
+        height:"100vh",
+        width:"100vw"
+    }}>
     
-        <div
-          style={{
-            backgroundColor: "white",
-            padding: "10px",
-            borderRadius: "5px",
-            // flexWrap:"wrap",
-          }}
-        >
-          <div
-            style={{
-              minWidth: "300px",
-              backgroundColor: "whitesmoke",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius:"5px"
-            //   padding: "10px",
-            }}
-          >
-            <p
-              style={{
-                margin: "5px 0px",
-                fontSize: "17px",
-                // fontWeight:"500"
-              }}
-            >
-              User Name: Ranjit
-            </p>
-            <p
-              style={{
-                // margin:"10px 0px",
-                fontSize: "17px",
-                // fontWeight:"500"
-              }}
-            >
-              Birth Date: 11 jan 2002
-            </p>
-            <p
-              style={{
-                margin: "5px 0px",
-                fontSize: "17px",
-                // fontWeight:"500"
-              }}
-            >
-              User Status: <span style={{ color: "green" }}>Active</span>
-            </p>
+    <nav className="sidebar" style={{
+        width:"250px",
+        backgroundColor:"gray",
+        height:"100vh",
+        marginTop:"10px"
+    }} >
+            <h2>Dashboard</h2>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Analytics</a></li>
+                <li><a href="#">Settings</a></li>
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">Logout</a></li>
+            </ul>
+        </nav>
 
-            <div
-              style={{
-                height: "100px",
-                backgroundColor: "green",
-                width: "100%",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "bottom",
-                marginTop:"20px",
-                // borderRadius:"x"
-              }}
-            >
-              
-            </div>
-          </div>
+        <div class="main-content">
+          
+            <header>
+                <h1>Welcome to Your Dashboard</h1>
+            </header>
+
+            
+            <section class="content">
+                <div class="card">
+                    <h3>Statistics</h3>
+                    <p>Views: 2,450</p>
+                    <p>Users: 1,200</p>
+                </div>
+                <div class="card">
+                    <h3>Revenue</h3>
+                    <p>This Month: $5,000</p>
+                    <p>Last Month: $4,500</p>
+                </div>
+                <div class="card">
+                    <h3>Tasks</h3>
+                    <p>Completed: 10</p>
+                    <p>Pending: 3</p>
+                </div>
+                <div class="card">
+                    <h3>Notifications</h3>
+                    <p>New Messages: 5</p>
+                    <p>Alerts: 2</p>
+                </div>
+            </section>
         </div>
+    
 
-        <div
-          style={{
-            backgroundColor: "white",
-            // width: "100%",
-            // height: "100vh",
-            padding: "10px",
-            borderRadius: "5px",
-          }}
-        >
-          <div
-            style={{
-              minWidth: "300px",
-              backgroundColor: "whitesmoke",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius:"5px"
-            //   padding: "10px",
-            }}
-          >
-            <p
-              style={{
-                margin: "5px 0px",
-                fontSize: "17px",
-                // fontWeight:"500"
-              }}
-            >
-              User Name: Ranjit
-            </p>
-            <p
-              style={{
-                // margin:"10px 0px",
-                fontSize: "17px",
-                // fontWeight:"500"
-              }}
-            >
-              Birth Date: 11 jan 2002
-            </p>
-            <p
-              style={{
-                margin: "5px 0px",
-                fontSize: "17px",
-                // fontWeight:"500"
-              }}
-            >
-              User Status: <span style={{ color: "green" }}>Active</span>
-            </p>
-
-            <div
-              style={{
-                height: "100px",
-                backgroundColor: "green",
-                width: "100%",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "bottom",
-                marginTop:"20px",
-                // borderRadius:"x"
-              }}
-            >
-              
-            </div>
-          </div>
-        </div>
-
-        <div
-          style={{
-            backgroundColor: "white",
-            // width: "100%",
-            // height: "100vh",
-            padding: "10px",
-            borderRadius: "5px",
-          }}
-        >
-          <div
-            style={{
-            //   maxWidth: "300px",
-              minWidth:"300px",
-              backgroundColor: "whitesmoke",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius:"5px"
-            //   padding: "10px",
-            }}
-          >
-            <p
-              style={{
-                margin: "5px 0px",
-                fontSize: "17px",
-                // fontWeight:"500"
-              }}
-            >
-              User Name: Ranjit
-            </p>
-            <p
-              style={{
-                // margin:"10px 0px",
-                fontSize: "17px",
-                // fontWeight:"500"
-              }}
-            >
-              Birth Date: 11 jan 2002
-            </p>
-            <p
-              style={{
-                margin: "5px 0px",
-                fontSize: "17px",
-                // fontWeight:"500"
-              }}
-            >
-              User Status: <span style={{ color: "green" }}>Active</span>
-            </p>
-
-            <div
-              style={{
-                height: "100px",
-                backgroundColor: "green",
-                width: "100%",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "bottom",
-                marginTop:"20px",
-                // borderRadius:"x"
-              }}
-            >
-              
-            </div>
-          </div>
-        </div>
-
-        <div
-          style={{
-            backgroundColor: "white",
-            // width: "100%",
-            // height: "100vh",
-            padding: "10px",
-            borderRadius: "5px",
-          }}
-        >
-          <div
-            style={{
-             
-              backgroundColor: "whitesmoke",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius:"5px",
-              minWidth:"300px"
-            //   padding: "10px",
-            }}
-          >
-            <p
-              style={{
-                margin: "5px 0px",
-                fontSize: "17px",
-                // fontWeight:"500"
-              }}
-            >
-              User Name: Ranjit
-            </p>
-            <p
-              style={{
-                // margin:"10px 0px",
-                fontSize: "17px",
-                // fontWeight:"500"
-              }}
-            >
-              Birth Date: 11 jan 2002
-            </p>
-            <p
-              style={{
-                margin: "5px 0px",
-                fontSize: "17px",
-                // fontWeight:"500"
-              }}
-            >
-              User Status: <span style={{ color: "green" }}>Active</span>
-            </p>
-
-            <div
-              style={{
-                height: "100px",
-                backgroundColor: "green",
-                // width: "100%",
-                minWidth:"300px",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "bottom",
-                marginTop:"20px",
-                // borderRadius:"x"
-              }}
-            >
-              
-            </div>
-          </div>
-        </div>
     </div>
-      </div>
-    </>
-  );
-}
+)}
